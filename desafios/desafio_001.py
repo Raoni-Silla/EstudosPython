c346f10039c0990cab025cde103382e254d11086
raoni silla <Raonisilla@gmail.com>
2025-06-20 12:35:34 -0300

    update

---
+++ desafios/desafio_001.py
@@ -0,0 +1,4 @@
+n1 = float(input('Digite um valor: '))
+n2 = float (input('Digite outro valor: '))
+Soma = n1 + n2
+print('a soma entre {} e {} é igual a {}'.format(n1,n2,Soma))
---
+++ desafios/desafio_002.py
@@ -0,0 +1,5 @@
+val = input('Digite um valor: ')
+print (type (val))
+print (val.isnumeric()) #verifica se todos os digitos são numericos
+print(val.isalpha()) #verifica se todos os digitos são letras
+print (val.isalnum()) #verifica se tem letras e numeros misturados
