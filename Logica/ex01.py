8b4bc42c9186bc19ba5bbec0ef6792afd37db914
raoni silla <Raonisilla@gmail.com>
2025-06-20 10:45:40 -0300

    update

---
+++ Logica/ex01.py
@@ -0,0 +1,2 @@
+nome = input('digite seu nome')
+print ('Boas vindas', nome ,'seja bem vindo ao nosso sistema')
---
+++ Logica/ex02.py
@@ -0,0 +1,6 @@
+Num = int (input('Digite um numero:'))
+Num2 = float (input ('Digite outro numero:'))
+soma = Num + Num2
+print ('a soma vale {}'.format(soma)) #format é mais facil de se usar
+print ('a soma vale', soma)# se usa ',' pra concatenar tipos de dados diferentes, e '+' se usa para concatenar strings
+print ('a soma vale' + str(soma)) # transforma a variavel soma em uma string
