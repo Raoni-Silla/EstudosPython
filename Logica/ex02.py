4323609396be1352d51f742246d0c5aa38d26a63
raoni silla <Raonisilla@gmail.com>
2025-06-20 12:00:42 -0300

    update

--- Logica/ex02.py
+++ Logica/ex02.py
@@ -3,4 +3,5 @@ Num2 = float (input ('Digite outro numero:'))
 soma = Num + Num2
 print ('a soma vale {}'.format(soma)) #format é mais facil de se usar
 print ('a soma vale', soma)# se usa ',' pra concatenar tipos de dados diferentes, e '+' se usa para concatenar strings
-print ('a soma vale' + str(soma)) # transforma a variavel soma em uma string
+print ('a soma vale ' + str(soma)) # transforma a variavel soma em uma string
+print(' a soma entre {} e {} vale {}'.format(Num, Num2, soma))
