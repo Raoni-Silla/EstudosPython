4b7f9d9c239571bd0b4e22ea508d39fcfa01ab84
raoni silla <Raonisilla@gmail.com>
2025-06-20 12:16:07 -0300

    update

---
+++ Logica/ex03.py
@@ -0,0 +1,7 @@
+n = bool(input('Digite um valor: '))
+print(n) #se tiver conteudo dentro é true se nao é falso
+N1 =  input('Digite um valor: ')
+print(N1.isnumeric())#função pergunta se a variavel é um numero, diz se é possivel converter esse valor em um numero
+N2 = input('digite um valor')
+print (N2.isalpha())#pergunta se a variavel é alpha, se são letras
+#metodos is são metodos de teste de tipo, lendo apenas com o input o tipo é sempre string
