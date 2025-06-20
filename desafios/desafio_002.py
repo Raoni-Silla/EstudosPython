82d675b71cf5804e392b450fab7df578478a263a
raoni silla <Raonisilla@gmail.com>
2025-06-20 12:40:24 -0300

    update

--- desafios/desafio_002.py
+++ desafios/desafio_002.py
@@ -1,5 +1,6 @@
 val = input('Digite um valor: ')
 print (type (val))
-print (val.isnumeric()) #verifica se todos os digitos são numericos
-print(val.isalpha()) #verifica se todos os digitos são letras
-print (val.isalnum()) #verifica se tem letras e numeros misturados
+print ('esse valor é numerico: ',val.isnumeric()) #verifica se todos os digitos são numericos
+print('esse valor é alfabetico:', val.isalpha()) #verifica se todos os digitos são letras
+print (' esse valor tem numeros e letras misturados:',val.isalnum()) #verifica se tem letras e numeros misturados
+print('ele é composto só de espaços', val.isspace())
