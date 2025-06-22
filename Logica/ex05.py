45400be4b6ddd03bd0435b35ace672dce1a61e6d
raoní silla <Raonisilla@gmail.com>
2025-06-22 17:17:19 -0300

    update

---
+++ Logica/ex05.py
@@ -0,0 +1,5 @@
+nome = input ('qual seu nome: ')
+print('prazer em te conhecer {:20}'.format(nome)) # escreve em 20 espaços
+print('prazer em te conhecer {:>20}'.format(nome)) #alinhado a direita em 20 espaços
+print('prazer em te conhecer {:^20}'.format(nome)) #centralizado em 20 espaços
+print('prazer em te conhecer {:=^20}'.format(nome))#vai ficar centralizado em 20 espaços com os iguais
