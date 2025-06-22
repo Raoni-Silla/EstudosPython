967feb424d1db461acd4e8085607d5c54dc2aa76
raoní silla <Raonisilla@gmail.com>
2025-06-22 18:35:12 -0300

    up

--- Logica/ex06.py
+++ Logica/ex06.py
@@ -2,7 +2,7 @@ n1 = int(input('Digite um numero: '))
 n2 = int(input('Digite outro numero: '))
 print('a soma vale {}'.format(n1+n2), end='>>> ')
 print('ola, olhe como a linha continuou')
-
+#
 
 
 
