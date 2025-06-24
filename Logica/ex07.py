007f88860fdadd6f946ce3f73c3110ac144c235a
raoní silla <Raonisilla@gmail.com>
2025-06-24 15:20:05 -0300

    update

---
+++ Logica/ex07.py
@@ -0,0 +1,5 @@
+#import math
+from math import sqrt, ceil
+num = int (input('Digite um número: '))
+print('a raiz quadrada é: {:.2f} '.format(sqrt(ceil(num))))
+#print('a raiz quadrada é: {:.2f} '.format(math.sqrt(num))) essa forma quando importa a biblioteca geral se usa o nome da biblioteca.metodo
