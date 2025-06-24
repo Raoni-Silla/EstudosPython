89d1829bff9f29de67c940d5a7141932bbe57824
raoní silla <Raonisilla@gmail.com>
2025-06-24 16:26:25 -0300

    update

---
+++ Logica/ex08.py
@@ -0,0 +1,8 @@
+import random
+import emoji
+num = random.random( )#o pc me da um numero aleatorio, um numero float entre 0 e 1
+print('{:.2f}'.format(num))
+num = random.randint(1,10)#randomiza um numero inteiro entre 1 a 10
+print(num)
+print (emoji.emojize("olá, mundo :coração_vermelho:",language='pt'))
+
